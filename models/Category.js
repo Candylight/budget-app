e4c06223be116075e52bac9fff02a0374d7d1d13
benjamin.hil <bhil@tradelab.fr>
2018-05-27 10:32:44 +0200

    #back: add priority to category

--- models/Category.js
+++ models/Category.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const CategorySchema = new mongoose.Schema({
   name: String,
   description: String,
+  priority: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
